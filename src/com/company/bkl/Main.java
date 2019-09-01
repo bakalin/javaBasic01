5b329e24ccdeef490a86893611dc0be7487f750d
bkl <760223776@qq.com>
2019-09-01 14:14:34 +0800

    git test01

--- src/com/company/bkl/Main.java
+++ src/com/company/bkl/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hello,java");
+        System.out.println("hello,java\nand git test");
+
     }
 }
