d9cbe8a4b6868d0ca91ae71e1cae45528502c932
bkl <760223776@qq.com>
2019-09-01 14:29:21 +0800

    a for loop

--- src/com/company/bkl/Main.java
+++ src/com/company/bkl/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello,java\nand git test");
-
+        int sum=0;
+        for (int i = 0; i <100 ; i++) {
+            sum=sum+i;
+        }
+        System.out.println(sum);
     }
 }
